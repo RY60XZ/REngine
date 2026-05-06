02df1ccede021fdd2026aab39fb6835b5bc9b31a
ryanyao <ryanyao1022@outlook.com>
2026-05-06 00:24:35 -0700

    feat: alpha-beta search

--- engine/search.py
+++ engine/search.py
@@ -4,28 +4,60 @@ from . import evaluation
 def find_best_move(board: chess.Board, depth: int) -> chess.Move | None:
     if depth == 0 or board.is_game_over():
         return None
-    legal_moves = list(board.legal_moves)
-    best_score = float("-inf")
+
+    maxplayer = True if board.turn == chess.WHITE else False
+    if maxplayer:
+        best_score = float("-inf")
+    else:
+        best_score = float("inf")
     best_move = None
-    for move in legal_moves:
+
+    for move in board.legal_moves:
         board.push(move)
-        score = - negamax(board, depth - 1)
-        if score > best_score:
-            best_score = score
-            best_move = move
+        score = alphabeta(board, depth - 1, float("-inf"), float("inf"))
         board.pop()
+        if maxplayer:
+            if score > best_score:
+                best_score = score
+                best_move = move
+        else:
+            if score < best_score:
+                best_score = score
+                best_move = move
+
     return best_move
 
-def negamax(board: chess.Board, depth: int) -> int:
-    #the function returns the side-to-move eval
+def alphabeta(board: chess.Board, depth: int, alpha, beta) -> int:
     if depth == 0 or board.is_game_over():
-        eval = evaluation.evaluate_board(board)
-        return eval if board.turn == chess.WHITE else -eval
-    legal_moves = list(board.legal_moves)
-    best_score = float("-inf")
-    for move in legal_moves:
-        board.push(move)
-        score = - negamax(board, depth - 1)
-        best_score = max(best_score, score)
-        board.pop()
-    return best_score
+        return evaluation.evaluate_board(board)
+    maxPlayer = True if board.turn == chess.WHITE else False
+
+    if maxPlayer: #White
+        best_score = float("-inf")
+        for move in board.legal_moves:
+            board.push(move)
+            score = alphabeta(board, depth-1, alpha, beta)
+            board.pop()
+            if score > best_score:
+                best_score = score
+            if score > alpha:
+                alpha = score
+            if alpha >= beta:
+                break
+
+        return best_score
+
+    else: #Black
+        best_score = float("inf")
+        for move in board.legal_moves:
+            board.push(move)
+            score = alphabeta(board, depth - 1, alpha, beta)
+            board.pop()
+            if score < best_score:
+                best_score = score
+            if score < beta:
+                beta = score
+            if alpha >= beta:
+                break
+
+        return best_score
