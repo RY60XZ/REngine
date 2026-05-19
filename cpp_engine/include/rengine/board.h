b6714f5978424c82682ec6b0ab1a671904146589
ryanyao <ryanyao1022@outlook.com>
2026-05-18 18:19:08 -0700

    feat: add board piece helper

--- cpp_engine/include/rengine/board.h
+++ cpp_engine/include/rengine/board.h
@@ -1,6 +1,7 @@
 #ifndef CPP_ENGINE_BOARD_H
 #define CPP_ENGINE_BOARD_H
 #include<rengine/types.h>
+#include<cassert>
 namespace rengine {
     struct Board {
         Bitboard pieces[2][6]{};
@@ -16,6 +17,19 @@ namespace rengine {
     };
 
     void clear(Board& board);
-    bool is_empty(const Board& board);
+    void set_piece(Board& board, Square square, Piece piece);
+    void remove_piece(Board&board, Square square);
+    void move_piece(Board&board, Square from_square, Square to_square); //assume to_square is empty
+    inline bool is_empty(const Board& board) {
+        return board.all==0;
+    }
+    inline Color color_of(Piece piece) {
+        assert(piece != NO_PIECE);
+        return static_cast<Color>((static_cast<int>(piece)-1)/6);
+    }
+    inline PieceType piece_type_of(Piece piece) {
+        assert(piece != NO_PIECE);
+        return static_cast<PieceType>((static_cast<int>(piece)-1)%6);
+    }
 }
 #endif //CPP_ENGINE_BOARD_H
--- cpp_engine/src/board.cpp
+++ cpp_engine/src/board.cpp
@@ -1,6 +1,6 @@
 #include "rengine/board.h"
 #include "rengine/types.h"
-
+#include "cassert"
 namespace rengine{
     void clear(Board& board) {
         for (auto& color_pieces : board.pieces) {
@@ -22,7 +22,36 @@ namespace rengine{
         board.full_move_number = 1;
     }
 
-    bool is_empty(const Board& board) {
-        return board.all == 0;
+    void set_piece(Board &board, Square square, Piece piece) {
+        assert(square>=0 && square<64);
+        assert(board.squares[square] == NO_PIECE);
+        int piece_color = color_of(piece), piece_type = piece_type_of(piece);
+        Bitboard pos_mask = 1ULL<<square;
+        board.pieces[piece_color][piece_type] |= pos_mask;
+        board.occupied[piece_color] |= pos_mask;
+        board.all |= pos_mask;
+        board.squares[square] = piece;
+    }
+
+    void remove_piece(Board &board, Square square) {
+        assert(square>=0 && square<64);
+        Piece piece = board.squares[square];
+        assert(piece != NO_PIECE);
+        int piece_color = color_of(piece), piece_type = piece_type_of(piece);
+        Bitboard pos_mask = ~(1ULL<<square);
+        board.pieces[piece_color][piece_type] &= pos_mask;
+        board.occupied[piece_color] &= pos_mask;
+        board.all &= pos_mask;
+        board.squares[square] = NO_PIECE;
+    }
+
+    void move_piece(Board &board, Square from_square, Square to_square) {
+        assert(from_square>=0 && from_square<64);
+        assert(to_square>=0 && to_square<64);
+        Piece from_piece = board.squares[from_square];
+        assert(from_piece != NO_PIECE);
+        assert(board.squares[to_square] == NO_PIECE);
+        remove_piece(board, from_square);
+        set_piece(board, to_square, from_piece);
     }
 }
