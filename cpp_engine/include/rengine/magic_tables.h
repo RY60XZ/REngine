1bc1d79ce07ddad789be44231f3278ef25df9b05
ryanyao <ryanyao1022@outlook.com>
2026-05-20 16:35:38 -0700

    refactor: flat tables for magic_tables

--- cpp_engine/include/rengine/magic_tables.h
+++ cpp_engine/include/rengine/magic_tables.h
@@ -2,18 +2,38 @@
 #define CPP_ENGINE_MAGIC_TABLE_H
 #include "rengine/types.h"
 #include <array>
+#include <cstddef>
 
 namespace rengine {
-    constexpr int BISHOP_INDEX_BITS = 11;
-    constexpr int ROOK_INDEX_BITS = 13;
+    struct MagicEntry {
+        Bitboard mask;
+        Bitboard magic;
+        int index_bits;
+        std::size_t offset;
+    };
 
-    extern const std::array<Bitboard, 64> rook_masks;
-    extern const std::array<Bitboard, 64> bishop_masks;
-    extern const std::array<Bitboard, 64> rook_magic;
-    extern const std::array<Bitboard, 64> bishop_magic;
+    constexpr std::size_t ROOK_ATTACK_TABLE_SIZE = 102400;
+    constexpr std::size_t BISHOP_ATTACK_TABLE_SIZE = 5248;
 
-    extern const std::array<std::array<Bitboard, 1 << BISHOP_INDEX_BITS>, 64> bishop_attacks;
-    extern const std::array<std::array<Bitboard, 1 << ROOK_INDEX_BITS>, 64> rook_attacks;
+    extern const std::array<MagicEntry, 64> rook_magic_entries;
+    extern const std::array<MagicEntry, 64> bishop_magic_entries;
+    extern const std::array<Bitboard, ROOK_ATTACK_TABLE_SIZE> rook_attacks;
+    extern const std::array<Bitboard, BISHOP_ATTACK_TABLE_SIZE> bishop_attacks;
+
+    inline std::size_t magic_index(Bitboard occupancy, const MagicEntry& entry) {
+        Bitboard blockers = occupancy & entry.mask;
+        return static_cast<std::size_t>((blockers * entry.magic) >> (64 - entry.index_bits));
+    }
+
+    inline Bitboard rook_attacks_from(Square square, Bitboard occupancy) {
+        const MagicEntry& entry = rook_magic_entries[square];
+        return rook_attacks[entry.offset + magic_index(occupancy, entry)];
+    }
+
+    inline Bitboard bishop_attacks_from(Square square, Bitboard occupancy) {
+        const MagicEntry& entry = bishop_magic_entries[square];
+        return bishop_attacks[entry.offset + magic_index(occupancy, entry)];
+    }
 }
 
 #endif //CPP_ENGINE_MAGIC_TABLE_H
--- cpp_engine/tools/find_magic.cpp
+++ cpp_engine/tools/find_magic.cpp
@@ -1,17 +1,21 @@
 #include "rengine/board.h"
-#include "rengine/types.h"
+#include "rengine/magic_tables.h"
 #include "rengine/square.h"
-#include<vector>
-#include<unordered_map>
-#include<array>
+#include "rengine/types.h"
+#include <array>
+#include <bit>
+#include <cassert>
 #include <fstream>
 #include <iomanip>
+#include <iostream>
+#include <unordered_map>
+#include <vector>
+
 using namespace std;
 using namespace rengine;
 
-//performance is not the biggest concern, since the script is for precomputation
-//still need to be reasonably fast, however
-
+// Performance is not the biggest concern, since the script is for precomputation.
+// It still needs to be reasonably fast, however.
 
 static Bitboard random_state = 1804289383ULL;
 
@@ -24,12 +28,10 @@ Bitboard random_u64() {
     return z ^ (z >> 31);
 }
 
-constexpr int BISHOP_INDEX_BITS = 11;
-constexpr int ROOK_INDEX_BITS = 13;
-
 constexpr bool is_on_board(int file, int rank) {
     return file >= 0 && file < 8 && rank >= 0 && rank < 8;
 }
+
 vector<Square> generate_bishop_squares(Square from) {
     vector<Square> bishop_squares;
     int from_file = file_of(from), from_rank = rank_of(from);
@@ -67,7 +69,7 @@ vector<Square> generate_bishop_squares(Square from) {
     return bishop_squares;
 }
 
-Bitboard generate_bishop_bitboard_mask(Square from, const vector<Square>& bishop_squares) {
+Bitboard generate_bishop_bitboard_mask(const vector<Square>& bishop_squares) {
     auto bishop_bitboard = Bitboard{0};
     for (auto s : bishop_squares) {
         bishop_bitboard |= Bitboard{1} << s;
@@ -112,7 +114,7 @@ vector<Square> generate_rook_squares(Square from) {
     return rook_squares;
 }
 
-Bitboard generate_rook_bitboard_mask(Square from, const vector<Square>& rook_squares) {
+Bitboard generate_rook_bitboard_mask(const vector<Square>& rook_squares) {
     auto rook_bitboard = Bitboard{0};
     for (auto s : rook_squares) {
         rook_bitboard |= Bitboard{1} << s;
@@ -120,8 +122,8 @@ Bitboard generate_rook_bitboard_mask(Square from, const vector<Square>& rook_squ
     return rook_bitboard;
 }
 
-void generate_blockage_bitboards(int i, Bitboard curr, const vector<Square>& possible_squares, vector<Bitboard>& result) {
-    if (i==possible_squares.size()) {
+void generate_blockage_bitboards(size_t i, Bitboard curr, const vector<Square>& possible_squares, vector<Bitboard>& result) {
+    if (i == possible_squares.size()) {
         result.push_back(curr);
         return;
     }
@@ -197,68 +199,34 @@ Bitboard scan_rook_attack(Square from, Bitboard blockage) {
     return attack;
 }
 
-Bitboard find_bishop_magic(Square from) {
-    auto bishop_squares = generate_bishop_squares(from);
-    vector<Bitboard> blockage_bitboards;
-    generate_blockage_bitboards(0, Bitboard{0}, bishop_squares, blockage_bitboards);
-    vector<Bitboard> attacks(blockage_bitboards.size());
-    for (int i = 0; i < blockage_bitboards.size(); ++i) {
-        attacks[i] = scan_bishop_attack(from, blockage_bitboards[i]);
-    }
-    unordered_map<Bitboard, Bitboard> hash; //first: hash  second: attack bitboard
-    Bitboard magic;
+Bitboard find_magic(const vector<Bitboard>& blockage_bitboards, const vector<Bitboard>& attacks, Bitboard mask, int index_bits) {
+    vector<Bitboard> used(size_t{1} << index_bits);
+    vector<bool> occupied(size_t{1} << index_bits);
+
     while (true) {
         bool flag = true;
-        magic = random_u64() & random_u64() & random_u64();
-        for (int i = 0; i < blockage_bitboards.size(); ++i) {
-            Bitboard key = (blockage_bitboards[i] * magic) >> (64 - BISHOP_INDEX_BITS);
-            if (hash.contains(key)) {
-                if (hash[key] != attacks[i]) {
+        Bitboard magic = random_u64() & random_u64() & random_u64();
+        if (popcount((mask * magic) & 0xFF00000000000000ULL) < 6) {
+            continue;
+        }
+        fill(occupied.begin(), occupied.end(), false);
+
+        for (size_t i = 0; i < blockage_bitboards.size(); ++i) {
+            auto key = static_cast<size_t>((blockage_bitboards[i] * magic) >> (64 - index_bits));
+            if (occupied[key]) {
+                if (used[key] != attacks[i]) {
                     flag = false;
                     break;
                 }
             }
             else {
-                hash[key] = attacks[i];
+                occupied[key] = true;
+                used[key] = attacks[i];
             }
-
         }
-        hash.clear();
-        if (flag) break;
-    }
-    return magic;
-}
 
-Bitboard find_rook_magic(Square from) {
-    auto rook_squares = generate_rook_squares(from);
-    vector<Bitboard> blockage_bitboards;
-    generate_blockage_bitboards(0, Bitboard{0}, rook_squares, blockage_bitboards);
-    vector<Bitboard> attacks(blockage_bitboards.size());
-    for (int i = 0; i < blockage_bitboards.size(); ++i) {
-        attacks[i] = scan_rook_attack(from, blockage_bitboards[i]);
+        if (flag) return magic;
     }
-    unordered_map<Bitboard, Bitboard> hash; //first: hash  second: attack bitboard
-    Bitboard magic;
-    while (true) {
-        bool flag = true;
-        magic = random_u64() & random_u64() & random_u64();
-        for (int i = 0; i < blockage_bitboards.size(); ++i) {
-            Bitboard key = (blockage_bitboards[i] * magic) >> (64 - ROOK_INDEX_BITS);
-            if (hash.contains(key)) {
-                if (hash[key] != attacks[i]) {
-                    flag = false;
-                    break;
-                }
-            }
-            else {
-                hash[key] = attacks[i];
-            }
-
-        }
-        hash.clear();
-        if (flag) break;
-    }
-    return magic;
 }
 
 void write_bitboard(ostream& out, Bitboard b) {
@@ -267,92 +235,92 @@ void write_bitboard(ostream& out, Bitboard b) {
         << "ULL" << dec;
 }
 
-array<Bitboard, 64> rook_masks;
-array<Bitboard, 64> bishop_masks;
-array<Bitboard, 64> rook_magic;
-array<Bitboard, 64> bishop_magic;
-array<array<Bitboard, 1<<BISHOP_INDEX_BITS>, 64> bishop_attacks{};
-array<array<Bitboard, 1<<ROOK_INDEX_BITS>, 64> rook_attacks{};
+void write_magic_entries(ostream& out, const char* name, const array<MagicEntry, 64>& entries) {
+    out << "const std::array<MagicEntry, 64> " << name << " = {{\n";
+    for (const MagicEntry& entry : entries) {
+        out << "    {";
+        write_bitboard(out, entry.mask);
+        out << ", ";
+        write_bitboard(out, entry.magic);
+        out << ", " << entry.index_bits << ", " << entry.offset << "},\n";
+    }
+    out << "}};\n\n";
+}
+
+template <size_t N>
+void write_attack_table(ostream& out, const char* name, const char* size_name, const array<Bitboard, N>& attacks) {
+    out << "const std::array<Bitboard, " << size_name << "> " << name << " = {{\n";
+    for (Bitboard b : attacks) {
+        out << "    ";
+        write_bitboard(out, b);
+        out << ",\n";
+    }
+    out << "}};\n\n";
+}
+
+array<MagicEntry, 64> generated_rook_magic_entries{};
+array<MagicEntry, 64> generated_bishop_magic_entries{};
+array<Bitboard, ROOK_ATTACK_TABLE_SIZE> rook_attack_table{};
+array<Bitboard, BISHOP_ATTACK_TABLE_SIZE> bishop_attack_table{};
 
 int main() {
+    size_t bishop_offset = 0;
+    size_t rook_offset = 0;
+
     for (Square s = 0; s < 64; ++s) {
         auto bishop_squares = generate_bishop_squares(s);
         vector<Bitboard> bishop_blockage_bitboards;
         generate_blockage_bitboards(0, Bitboard{0}, bishop_squares, bishop_blockage_bitboards);
-        bishop_masks[s] = generate_bishop_bitboard_mask(s, bishop_squares);
-        bishop_magic[s] = find_bishop_magic(s);
-        for (auto b : bishop_blockage_bitboards) {
-            bishop_attacks[s][(b * bishop_magic[s]) >> (64 - BISHOP_INDEX_BITS)] = scan_bishop_attack(s, b);
+        vector<Bitboard> bishop_scan_attacks(bishop_blockage_bitboards.size());
+        for (size_t i = 0; i < bishop_blockage_bitboards.size(); ++i) {
+            bishop_scan_attacks[i] = scan_bishop_attack(s, bishop_blockage_bitboards[i]);
         }
+
+        int bishop_index_bits = static_cast<int>(bishop_squares.size());
+        Bitboard bishop_mask = generate_bishop_bitboard_mask(bishop_squares);
+        Bitboard bishop_magic = find_magic(bishop_blockage_bitboards, bishop_scan_attacks, bishop_mask, bishop_index_bits);
+        generated_bishop_magic_entries[s] = {bishop_mask, bishop_magic, bishop_index_bits, bishop_offset};
+        for (size_t i = 0; i < bishop_blockage_bitboards.size(); ++i) {
+            auto key = static_cast<size_t>((bishop_blockage_bitboards[i] * bishop_magic) >> (64 - bishop_index_bits));
+            bishop_attack_table[bishop_offset + key] = bishop_scan_attacks[i];
+        }
+        bishop_offset += size_t{1} << bishop_index_bits;
+
         auto rook_squares = generate_rook_squares(s);
         vector<Bitboard> rook_blockage_bitboards;
         generate_blockage_bitboards(0, Bitboard{0}, rook_squares, rook_blockage_bitboards);
-        rook_masks[s] = generate_rook_bitboard_mask(s, rook_squares);
-        rook_magic[s] = find_rook_magic(s);
-        for (auto b : rook_blockage_bitboards) {
-            rook_attacks[s][(b * rook_magic[s]) >> (64 - ROOK_INDEX_BITS)] = scan_rook_attack(s, b);
+        vector<Bitboard> rook_scan_attacks(rook_blockage_bitboards.size());
+        for (size_t i = 0; i < rook_blockage_bitboards.size(); ++i) {
+            rook_scan_attacks[i] = scan_rook_attack(s, rook_blockage_bitboards[i]);
         }
-    }
-    ofstream out("src/magic_tables.cpp");
-    out << "#include \"rengine/magic_tables.h\"\n\n";
-    out << "namespace rengine {\n\n";
 
-    out << "const std::array<Bitboard, 64> rook_masks = {{\n";
-    for (Bitboard b : rook_masks) {
-        out << "    ";
-        write_bitboard(out, b);
-        out << ",\n";
-    }
-    out << "}};\n\n";
-
-    out << "const std::array<Bitboard, 64> bishop_masks = {{\n";
-    for (Bitboard b : bishop_masks) {
-        out << "    ";
-        write_bitboard(out, b);
-        out << ",\n";
-    }
-    out << "}};\n\n";
-
-    out << "const std::array<Bitboard, 64> rook_magic = {{\n";
-    for (Bitboard b : rook_magic) {
-        out << "    ";
-        write_bitboard(out, b);
-        out << ",\n";
-    }
-    out << "}};\n\n";
-
-    out << "const std::array<Bitboard, 64> bishop_magic = {{\n";
-    for (Bitboard b : bishop_magic) {
-        out << "    ";
-        write_bitboard(out, b);
-        out << ",\n";
-    }
-    out << "}};\n\n";
-
-    out << "const std::array<std::array<Bitboard, 1 << ROOK_INDEX_BITS>, 64> rook_attacks = {{\n";
-    for (const auto& row : rook_attacks) {
-        out << "    {{\n";
-        for (Bitboard b : row) {
-            out << "        ";
-            write_bitboard(out, b);
-            out << ",\n";
+        int rook_index_bits = static_cast<int>(rook_squares.size());
+        Bitboard rook_mask = generate_rook_bitboard_mask(rook_squares);
+        Bitboard rook_magic = find_magic(rook_blockage_bitboards, rook_scan_attacks, rook_mask, rook_index_bits);
+        generated_rook_magic_entries[s] = {rook_mask, rook_magic, rook_index_bits, rook_offset};
+        for (size_t i = 0; i < rook_blockage_bitboards.size(); ++i) {
+            auto key = static_cast<size_t>((rook_blockage_bitboards[i] * rook_magic) >> (64 - rook_index_bits));
+            rook_attack_table[rook_offset + key] = rook_scan_attacks[i];
         }
-        out << "    }},\n";
+        rook_offset += size_t{1} << rook_index_bits;
     }
-    out << "}};\n\n";
 
-    out << "const std::array<std::array<Bitboard, 1 << BISHOP_INDEX_BITS>, 64> bishop_attacks = {{\n";
-    for (const auto& row : bishop_attacks) {
-        out << "    {{\n";
-        for (Bitboard b : row) {
-            out << "        ";
-            write_bitboard(out, b);
-            out << ",\n";
+        assert(bishop_offset == BISHOP_ATTACK_TABLE_SIZE);
+        assert(rook_offset == ROOK_ATTACK_TABLE_SIZE);
+
+        ofstream out("src/magic_tables.cpp");
+        if (!out) {
+            cerr << "failed to open src/magic_tables.cpp\n";
+            return 1;
         }
-        out << "    }},\n";
-    }
-    out << "}};\n\n";
 
-    out << "} // namespace rengine\n";
-    return 0;
+        out << "#include \"rengine/magic_tables.h\"\n\n";
+        out << "namespace rengine {\n\n";
+        write_magic_entries(out, "rook_magic_entries", generated_rook_magic_entries);
+        write_magic_entries(out, "bishop_magic_entries", generated_bishop_magic_entries);
+        write_attack_table(out, "rook_attacks", "ROOK_ATTACK_TABLE_SIZE", rook_attack_table);
+        write_attack_table(out, "bishop_attacks", "BISHOP_ATTACK_TABLE_SIZE", bishop_attack_table);
+        out << "} // namespace rengine\n";
+        return 0;
+
 }
